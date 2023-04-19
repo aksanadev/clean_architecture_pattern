9ca643db6c3a0ed1a1ae05dc0749772edeecaf60
aksanadev <aksana.dev@gmail.com>
2023-04-18 22:55:42 -0500

    connected enity to model and then connected it to an api to the usecase and repo

--- lib/data/datasources/advice_remote_datasource.dart
+++ lib/data/datasources/advice_remote_datasource.dart
@@ -1,14 +1,25 @@
+import 'dart:convert';
+import 'package:clean_architecture_pattern/data/models/advice_model.dart';
+import 'package:http/http.dart' as http;
 
-abstract class AdviceRemoteDataSource {
+abstract class AdviceRemoteDatasource {
   /// request a random advice from api
   /// returns [AdviceModel] if successful
   /// throws a [ServerException] if statusCode != 200
   Future<AdviceModel> getRandomAdviceFromApi();
 }
 
-class AdviceRemoteDataSourceImpl implements AdviceRemoteDataSource {
+class AdviceRemoteDatasourceImpl implements AdviceRemoteDatasource {
+  final client = http.Client();
   @override
-  Future<AdviceModel> getRandomAdviceFromApi() {
-    throw UnimplementedError();
+  Future<AdviceModel> getRandomAdviceFromApi() async {
+    final response = await client.get(
+      Uri.parse('https://api.flutter-community.com/api/v1/advice'),
+      headers: {
+        'content-type': 'application/json',
+      },
+    );
+    final responseBody = json.decode(response.body);
+    return AdviceModel.fromJson(responseBody);
   }
 }
--- lib/data/repositories/advice_repo_impl.dart
+++ lib/data/repositories/advice_repo_impl.dart
@@ -1,12 +1,15 @@
+import 'package:clean_architecture_pattern/data/datasources/advice_remote_datasource.dart';
 import 'package:clean_architecture_pattern/domain/failures/failures.dart';
 import 'package:clean_architecture_pattern/domain/entities/advice_entity.dart';
 import 'package:clean_architecture_pattern/domain/repositories/advice_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class AdviceRepoImpl implements AdviceRepo {
+  final AdviceRemoteDatasource adviceRemoteDatasource =
+      AdviceRemoteDatasourceImpl();
   @override
-  Future<Either<Failure, AdviceEntity>> getAdviceFromDataSource() {
-    throw UnimplementedError();
+  Future<Either<Failure, AdviceEntity>> getAdviceFromDataSource() async {
+    final result = await adviceRemoteDatasource.getRandomAdviceFromApi();
+    return right(result);
   }
-  
 }
